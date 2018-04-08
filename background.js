780a7cf0b9f8391697ab14c44e812bfdc6ace38b
Nicholas Palenchar <nickpalenchar@users.noreply.github.com>
2018-04-08 09:27:23 -0400

    chpt

--- background.js
+++ background.js
@@ -1,19 +1,14 @@
 function onClicked (tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    var input = document.createElement('input');
-    input.style.position = 'fixed';
-    input.style.opacity = 0;
-    input.value = "¯\\_(ツ)_/¯";
-    document.body.appendChild(input);
-    input.select();
-    document.execCommand('Copy');
-    document.body.removeChild(input);
-    chrome.browserAction.setIcon({path:"icon_green.png"});
-
-    setTimeout(resetIcon, 2530);
-  });
+  var input = document.createElement('input');
+  input.style.position = 'fixed';
+  input.style.opacity = 0;
+  input.value = "¯\\_(ツ)_/¯";
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand('Copy');
+  document.body.removeChild(input);
+  chrome.browserAction.setIcon({path:"icon_green.png"});
+  setTimeout(resetIcon, 2530);
 }
 
 function resetIcon() {
